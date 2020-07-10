a10bc4494310d085c99f75d800883faf7af168ac
yoga-nugroho129 <yoga.nugroho129@gmail.com>
2020-07-10 07:54:03 +0700

    push to github

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav()
+  $('.materialboxed').materialbox()
+  $('.parallax').parallax()
+  $('.tabs').tabs()
+  $('.datepicker').datepicker({
+    disableWeekends: true
+  })
+  $('.tooltipped').tooltip()
+  $('.scrollspy').scrollSpy()
+});
